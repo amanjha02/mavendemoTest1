f7a602681f4a01dda521aa32f3a1b356ebc66ef2
Aman Kumar Jha <ajha@hsdyn.com>
2019-08-28 14:30:23 +0530

    First Message

---
+++ src/test/java/com/qa/TestPackage/DemoTest2.java
@@ -0,0 +1,36 @@
+package com.qa.TestPackage;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	@Test
+	public void sum() {
+		int a=10;
+		int b=30;
+		Assert.assertEquals(40, a+b);
+		System.out.println("pass");
+	}
+	@Test
+	public void sub() {
+		int a=10;
+		int b=30;
+		Assert.assertEquals(20, b-a);
+		System.out.println("pass");
+	}
+	@Test
+	public void multiply() {
+		int a=10;
+		int b=30;
+		Assert.assertEquals(300, a*b);
+		System.out.println("pass");
+	}
+	@Test
+	public void divide() {
+		int a=10;
+		int b=30;
+		Assert.assertEquals(3, b/a);
+		System.out.println("pass");
+	}
+
+}
